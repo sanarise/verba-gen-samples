d87b4fd9556fcc6a34eb8387aaa4da7df25b189b
Aleksandr Chupakhin <sanarise@yandex.ru>
2018-12-09 15:05:44 +0300

    Add help via comander.js

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 run:
-	npx babel-node -- 'src/bin/verba-gen-samples.js'
+	npx babel-node -- 'src/bin/verba-gen-samples.js' $(I) $(O)
 
 build:
 	rm -rf dist
--- src/bin/verba-gen-samples.js
+++ src/bin/verba-gen-samples.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log('Redy to build!');
+import commander from 'commander';
+import { version } from '../../package.json';
+
+const processAction = (inputJson, outputFile) => {
+  console.log('args:', inputJson, outputFile);
+  console.log('opts:', commander.format);
+};
+
+commander
+  .description('Generate textile samples markup.')
+  .option('-f, --format [type]', 'output format', 'md')
+  .version(version)
+  .arguments('<input_json> <output_file>')
+  .action(processAction)
+  .parse(process.argv);
+
+if (commander.args.length === 0) {
+  commander.help();
+}
